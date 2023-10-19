75b2bdf563fee771dbcdb4dc55e8340c1a7e715b
Usama Iqbal <usamaiqbaljr@gmail.com>
2023-10-18 23:27:12 -0700

    Initial commit for sas

--- Models/Projects/License.cs
+++ Models/Projects/License.cs
@@ -1,10 +1,15 @@
-﻿namespace Project_Management.Models.Projects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Management.Models.Projects
 {
     public class License
     {
        public int Id { get; set; }
+        [Display(Name = "License Name")]
         public string Name { get; set; }
+        [Display(Name = "Project Id")]
         public string Project_id { get; set; }
+        [Display(Name = "Expiry Date")]
         public DateTime Expiry_Date { get; set; }
     }
 }
--- Models/Projects/Project.cs
+++ Models/Projects/Project.cs
@@ -1,10 +1,15 @@
-﻿namespace Project_Management.Models.Projects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Management.Models.Projects
 {
     public class Project
     {
         public int Id { get; set; }
+        [Display(Name = "Project Id")]
         public string Project_Id { get; set; }
+        [Display(Name = "Project Name")]
         public string Project_name { get; set; }
+        [Display(Name  = "Start Date")]
         public DateTime Start_Date { get; set; }
         public string Description { get; set; }
         public int User_Id { get; set; }
